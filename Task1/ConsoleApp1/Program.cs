293d673450b54689e683b7005ed232108f84d2a7
Ale}{ <ashubenok710@gmail.com>
2022-06-21 11:11:18 +0300

    feat: added timer

--- Task1/ConsoleApp1/Program.cs
+++ Task1/ConsoleApp1/Program.cs
@@ -1,57 +1,17 @@
-﻿class Program
+﻿using System;
+using System.Threading;
+class Program
 {
-    class User
+    public static System.Timers.Timer timer = new System.Timers.Timer();
+
+    public static void TimerHandler(User user)
     {
-        public string firstName = "";
-        public string lastName = "";
-        public DateTime birthday;
-        
-        public User() 
-        {
-            this.firstName = "Не определено";
-            this.lastName = "Не определено";
-            this.birthday = DateTime.Now;
-        }    
-
-        public User(string str) : this()
-        {
-            char[] delimiterChars = { '#', ' ', '/', ',', '.', ':', '\t' };
-            string[] data = str.Split(delimiterChars);
-
-            if (data.Length == 5)
-            {
-                this.firstName = data[0];
-                this.lastName = data[1];
-                this.birthday = DateTime.ParseExact(data[2] + "-" + data[3] + "-" + data[4], "dd-MM-yyyy", System.Globalization.CultureInfo.InvariantCulture);                
-            }            
-        }
-
-        public override string ToString() 
-        {
-            return "Имя: " + this.firstName + "\n" +
-                "Фамилия: " + this.lastName + "\n" +
-                "Родился: " + this.getBirthdayAsString() + "\n" +
-                "Кол-во полных лет: " + this.getAge() + "\n" + 
-                "Следующий день рождение через: " + this.getPeriodToNextBirthday();
-        }
-        private int getAge() {
-            return DateTime.Now.Year - this.birthday.Year;
-        }
-        private string getPeriodToNextBirthday() 
-        {
-            TimeSpan span = this.birthday.AddYears(this.getAge() + 1) - DateTime.Now;
-            return String.Format("{0} days, {1} hours, {2} minutes, {3} seconds",
-                span.Days, span.Hours, span.Minutes, span.Seconds);
-        }
-        private string getBirthdayAsString() 
-        {
-            return String.Format("{0:dddd dd MMMM yyyy}", this.birthday);                         
-        }
+        Console.WriteLine(user.getPeriodToNextBirthday());
     }
 
     static void Main(string[] args)
     {
-        System.Console.WriteLine($"Пользователь, введите: Имя, Фамилию, Год рождения. Разделителем между частями может быть любой символ: пробел, #, / и так далее.");
+        System.Console.WriteLine($ "Пользователь, введите: Имя, Фамилию, Год рождения. Разделителем между частями может быть любой символ: пробел, #, / и так далее.");
         //Сергей.Осипенко.07/04/1997
 
         string str = "";
@@ -63,13 +23,28 @@
             {
                 break;
             }
-            else 
+            else
             {
-                System.Console.WriteLine($"Первоначальная строка не может быть пустой или больше 40 символов.");
+                System.Console.WriteLine($ "Первоначальная строка не может быть пустой или больше 40 символов.");
             }
         }
 
         User user = new User(str);
-        System.Console.WriteLine($"{user.ToString()}");
+        Console.WriteLine($ "{user.ToString()}");
+
+        timer.Elapsed += (sender, eventArgs) => {
+            if (user.isBirthdayToday() & DateTime.Now.Minute == 33)
+            {
+                Console.WriteLine("Поздравляем вам исполнилось " + user.getAge());
+                timer.Stop();
+                Environment.Exit(0);
+            }
+            TimerHandler(user);
+        };
+        timer.Interval = (1000);
+        timer.Enabled = true;
+        timer.Start();
+        Console.ReadKey();
     }
+
 }
---
+++ Task1/ConsoleApp1/User.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+public class User
+{
+    public string firstName = "";
+    public string lastName = "";
+    public DateTime birthday;
+
+    public User()
+    {
+        this.firstName = "Не определено";
+        this.lastName = "Не определено";
+        this.birthday = DateTime.Now;
+    }
+
+    public User(string str) : this()
+    {
+        char[] delimiterChars = {
+      '#',
+      ' ',
+      '/',
+      ',',
+      '.',
+      ':',
+      '\t'
+    };
+        string[] data = str.Split(delimiterChars);
+
+        if (data.Length > 5)
+        {
+            this.firstName = data[0];
+            this.lastName = data[1];
+            this.birthday = DateTime.ParseExact(data[2] + "-" + data[3] + "-" + data[4], "dd-MM-yyyy", System.Globalization.CultureInfo.InvariantCulture);
+        }
+    }
+
+    public override string ToString()
+    {
+        return "Имя: " + this.firstName + "\n" +
+          "Фамилия: " + this.lastName + "\n" +
+          "Родился: " + this.getBirthdayAsString() + "\n" +
+          "Кол-во полных лет: " + this.getAge() + "\n" +
+          "Следующий день рождение через: " + this.getPeriodToNextBirthday();
+    }
+    public int getAge()
+    {
+        return DateTime.Now.Year - this.birthday.Year;
+    }
+    public string getPeriodToNextBirthday()
+    {
+        DateTime today = DateTime.Today;
+        DateTime next = birthday.AddYears(today.Year - birthday.Year);
+
+        if (next.Date <= today.Date)
+            next = next.AddYears(1);
+
+        TimeSpan span = next - DateTime.Now;
+        return String.Format("{0} days, {1} hours, {2} minutes, {3} seconds",
+          span.Days, span.Hours, span.Minutes, span.Seconds);
+    }
+    private string getBirthdayAsString()
+    {
+        return String.Format("{0:dddd dd MMMM yyyy}", this.birthday);
+    }
+
+    public bool isBirthdayToday()
+    {
+        if (this.birthday.Day == DateTime.Now.Day & this.birthday.Month == DateTime.Now.Month)
+        {
+            return true;
+        }
+        return false;
+    }
+}
