b3199b6816036060f0a78c7350f1d46d84144013
Ale}{ <ashubenok710@gmail.com>
2022-09-20 15:15:33 +0300

    dev: identity app

---
+++ Task5/CinemaPortalApp.Identity/Data/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using AuthenticationServer.Models;
+using CinemaPortal.Identity.Models;
+
+namespace CinemaPortal.Identity.Data;
+
+public interface IUserRepository
+{
+    public Task<UserProfile> GetByEmailAsync(string email);
+}
---
+++ Task5/CinemaPortalApp.Identity/DbContexts/AuthDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CinemaPortal.Identity.Models;
+
+namespace AuthenticationServer.DbContexts;
+
+public class AuthDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=.;Database=mydb;Trusted_Connection=True;");
+    }
+
+    public DbSet<UserProfile> UserProfile { get; set; }
+}
